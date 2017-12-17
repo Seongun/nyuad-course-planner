19aa39bd42f5ccac4611a337e0029167f9c0bb47
seongunsi <seongun@blockchainofthings.com>
2017-12-17 12:12:48 +0400

    moved to Linux, adding features to scraper to collect school based collections

--- scraper/headlessScraper.js
+++ scraper/headlessScraper.js
@@ -1,6 +1,12 @@
 const puppeteer = require('puppeteer');
 const CREDS = require('./creds');
 
+// await Promise.all([
+//   page.click('.load_more a'),
+//   page.waitForNavigation({ waitUntil: 'networkidle' })
+// ]);
+
+
 async function run() {
   const browser = await puppeteer.launch({
   headless: false
@@ -30,46 +36,138 @@ async function run() {
 	//main albert is crazy. We'll take the easy way by looking into mobile.
 	var url= 'https://m.albert.nyu.edu/app/catalog/classSearch';
 	await page.goto(url, {waitUntil:'networkidle2'});
-	
+		
 
-	const lenOptions = await page.evaluate(() => document.querySelectorAll("[data-id]").length);
-	console.log(lenOptions);
+	const LENGTH_SELECTOR_CLASS = 'term'
+
+	var listLength = await page.evaluate((sel) => {
+	    return document.getElementById(sel).length;
+	  }, LENGTH_SELECTOR_CLASS);
+
 	const LIST_TERM_SELECTOR = '#term > option:nth-child(INDEX)';
-
-	console.log(lenOptions);
-	
-
+	console.log(listLength);
+	semesterArr= [];
 
 
-	arr= [];
-
-	for (let i = 1; i <= lenOptions; i++) {
+	for (let i = 2; i <= listLength; i++) {
     // change the index to the next child
 	    let termSelector = LIST_TERM_SELECTOR.replace("INDEX", i);
-
 	    let termNum = await page.evaluate((sel) => {
 	        return document.querySelector(sel).getAttribute('value');
-	      }, usernameSelector);
-	    arr.push(termNum);
+	      }, termSelector);
+	    semesterArr.push(termNum);
 
  	 }
 
+ 	 // arr has the id of the semesters for which classes exist.
+ 	 // each semester Id can be used as following: https://m.albert.nyu.edu/app/catalog/classSearch/{semesterId}
+ 	 
 		
+	const SEARCH_GROUP_SELECTOR_CLASS = 'search-acad-group'
+	const LIST_SCHOOL_SELECTOR = '#search-acad-group > option:nth-child(INDEX)';
+	const SUBJECT_SELECTOR='#subject';
+	// const LIST_SCHOOL_SELECTOR = '#search-acad-group > option:nth-child(INDEX)';
+	const LIST_SUBJECT_SELECTOR = '#subject > option:nth-child(INDEX)';
+
+	const LOAD_CLASS_BUTTON_SELECTOR='#buttonSearch';
+	const CLASS_SEARCH_RESULTS_SELECTOR='#search-results';
+	const LOAD_SCHOOL_BUTTON_SELECTOR='#search-options > div:nth-child(6) > button';
+	const LOAD_SUBJECT_BUTTON_SELECTOR='#search-options > div.pull-left > div > button'
+
+	var AllClassesAllYears =[];
+
+	// loop through each semester's search page
+	for(var i=0; i<semesterArr.length; i++){
+		// console.log(i);
+		var semester=[];
+
+		//enter the semester search page
+		await page.goto(url+'/'+semesterArr[i], {waitUntil:'networkidle2'} );
+		
+		//get a list of academic group ( school names ) available
+		var groupListLength = await page.evaluate((sel) => {
+		    return document.getElementById(sel).length;
+		 }, SEARCH_GROUP_SELECTOR_CLASS);
+
+		var schoolArr=[];
+
+		var schoolObj={
+			name: null,
+			classes: [],
+		};
+
+		//loop through the academic groups available.
+		for (let i = 2; i <= groupListLength; i++) {
+
+	    // change the index to the next child
+		    let schoolSelector = LIST_SCHOOL_SELECTOR.replace("INDEX", i);
+
+		    let schoolName = await page.evaluate((sel) => {
+		        return document.querySelector(sel).getAttribute('value');
+		      }, schoolSelector);
+
+		    //select the school option
+		    page.select('select#search-acad-group', schoolName);
+
+		    await page.click(LOAD_SCHOOL_BUTTON_SELECTOR);
 
 
-	for(var i=0; i<array.length; i++){
-		await page.goto(url+'/'+array[i]);
+
+		 //    var subjectOptionsLength = await page.evaluate((sel) => {
+			//     return document.getElementById(sel).length;
+			// }, SUBJECT_SELECTOR);
+
+			// for(let l=2; l<=subjectOptionsLength; l++){
+
+			// 	let subjectSelector = LIST_SUBJECT_SELECTOR.replace("INDEX", i);
+		 //    	let subjectName = await page.evaluate((sel) => {
+			//         return document.querySelector(sel).getAttribute('value');
+			//     }, subjectSelector);
+
+			// }
 
 
-		// const form = await page.$('form-selector');
-		// await form.evaluate(form => form.submit());
-		await page.select('#term', 'my-value')
-		//checkout #search-acad-group dropdown select options,
-		// then choose on the #subject dropdown that renders options based on the previous options given
-		// ensure that "show open classes only is ticked off-- #filter-enrl-stat"
-		// tickedhen click #buttonSearch, wait for load.
 
-		await page.waitForNavigation(); 
+		    var subjectOptionsLength = await page.evaluate( (sel) => {
+		        return document.querySelector(sel).length;
+		      }, SUBJECT_SELECTOR);
+
+
+		    console.log(subjectOptionsLength);
+		    for(let j=2; j<=subjectOptionsLength; j++){
+		    	// console.log(subjectOptions[j].value);
+
+		    	let subjectSelector = LIST_SUBJECT_SELECTOR.replace("INDEX", j);
+
+			    let subjectValue = await page.evaluate((sel) => {
+			        return document.querySelector(sel).getAttribute('value');
+			      }, subjectSelector);
+
+			    // console.log(subjectValue);
+
+		    	page.select('select#subject', subjectValue);
+
+		    	await page.click(LOAD_SUBJECT_BUTTON_SELECTOR);
+				await page.click(LOAD_CLASS_BUTTON_SELECTOR);
+
+				var classesArr = await page.evaluate( (sel) => {
+					return document.getElementById(sel);
+				}, CLASS_SEARCH_RESULTS_SELECTOR );
+				console.log(classesArr);
+
+				var urlClasses ;
+				var resultValue;
+				for(let k=0; k<classesArr.length; k++){
+					
+					urlClasses = classesArr[k].getAttribute('href');
+					resultValue = urlClasses.slice(urlClasses.lastIndexOf('/'));
+					console.log(resultValue);
+					schoolObj.classes.push(resultValue);
+				}
+		    }
+	 	 }
+	 	 console.log(schoolObj);
+
 
 	}
 
